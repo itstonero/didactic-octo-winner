c8dee67034edb5da7749a6d9617567f7a0668645
Asadu Anthony <asadu.benedict@gmail.com>
2021-05-02 23:44:36 +0100

    Link the Logic Controllers into the router

---
+++ controllers/employee.js
@@ -0,0 +1,8 @@
+
+const getEmployees = () => {}
+const getEmployee = (employeeID) => {}
+const createEmployee = (employee) => {}
+const updateEmployee = (employee) => {}
+const deleteEmployee = (employeeID) => {}
+
+module.exports = { getEmployees, getEmployee, updateEmployee, deleteEmployee, createEmployee }
--- router/employee.js
+++ router/employee.js
@@ -1,43 +1,44 @@
 const express = require("express");
+const employeeController = require("../controllers/employee")
 const router = express.Router({ mergeParams: true });
 
 router.get('/',  async(request, response) => {
     try{
-        return response.json({ message: "OK"})
+        return response.json(employeeController.getEmployees())
     }catch(error){
-        return response.status(400).json({ message: 'Something went wrong'})
+        return response.status(400).json({ error})
     }
 })
 
 router.get('/:employeeID',  async(request, response) => {
     try{
-        return response.json({ message: "OK"})
+        return response.json(employeeController.getEmployee(request.params.employeeID))
     }catch(error){
-        return response.status(400).json({ message: 'Something went wrong'})
+        return response.status(400).json({ error})
     }
 })
 
 router.post('/',  async(request, response) => {
     try{
-        return response.json({ message: "OK"})
+        return response.status(201).json(employeeController.createEmployee(request.body));
     }catch(error){
-        return response.status(400).json({ message: 'Something went wrong'})
+        return response.status(400).json({ error})
     }
 })
 
 router.put('/',  async(request, response) => {
     try{
-        return response.json({ message: "OK"})
+        return response.json(employeeController.updateEmployee(request.body))
     }catch(error){
-        return response.status(400).json({ message: 'Something went wrong'})
+        return response.status(400).json({ error})
     }
 })
 
 router.delete('/:employeeID',  async(request, response) => {
     try{
-        return response.json({ message: "OK"})
+        return response.json(employeeController.deleteEmployee(request.params.employeeID))
     }catch(error){
-        return response.status(400).json({ message: 'Something went wrong'})
+        return response.status(400).json({ error})
     }
 })
 
